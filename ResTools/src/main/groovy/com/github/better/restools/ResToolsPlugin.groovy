a74d2d666ea28d37a9cfb869b0118dd653a63ebf
huazidev <huazi.dev@gmail.com>
2020-08-28 18:03:10 +0800

    refactor: replace `<<` with doLast, because `<<` was deprecated in Gradle 4.x and removed in Gradle 5.0, update AGP version.

--- ResTools/src/main/groovy/com/github/better/restools/ResToolsPlugin.groovy
+++ ResTools/src/main/groovy/com/github/better/restools/ResToolsPlugin.groovy
@@ -1,15 +1,8 @@
 package com.github.better.restools
 
-import com.github.better.restools.folder.AnimReplace
-import com.github.better.restools.folder.ColorReplace
-import com.github.better.restools.folder.DrawableReplace
-import com.github.better.restools.folder.LayoutReplace
-import com.github.better.restools.folder.MenuReplace
-import com.github.better.restools.folder.MipmapReplace
-import com.github.better.restools.folder.RawReplace
-import com.github.better.restools.folder.XmlReplace
-import com.github.better.restools.values.ValuesReplace
 import com.github.better.restools.ResToolsConfiguration
+import com.github.better.restools.folder.*
+import com.github.better.restools.values.ValuesReplace
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -32,60 +25,63 @@ class ResToolsPlugin implements Plugin<Project> {
         project.extensions.create('resConfig', ResToolsConfiguration.class)
 
         // === Create Task
-        project.tasks.create(["name": "replaceResName", "group": "resourceTools"]) << {
-            if (!project.android) {
-                throw new IllegalStateException('Must apply \'com.android.application\' or \'com.android.library\' first!')
+        project.tasks.create(["name": "replaceResName", "group": "resourceTools"]) {
+            doLast {
+                if (!project.android) {
+                    throw new IllegalStateException('Must apply \'com.android.application\' or \'com.android.library\' first!')
+                }
+
+                if (project.resConfig == null) {       // check config
+                    throw new IllegalArgumentException(
+                            'ResTools gradle plugin "resConfig DSL" config can not be null.')
+                }
+
+                // === System default
+                String sourceFolder = project.android.sourceSets.main.java.srcDirs[0].getAbsolutePath()
+                String resFolder = project.android.sourceSets.main.res.srcDirs[0].getAbsolutePath()
+                String manifestFilePath = project.android.sourceSets.main.manifest.srcFile.getAbsolutePath()
+
+                long startTime = System.currentTimeMillis()     // startTime
+
+                // === User settings
+                def config = project.resConfig
+                if (config.new_prefix == null || config.new_prefix.trim().length() == 0) {
+                    throw new IllegalArgumentException(
+                            'the [new_prefix] can not be null (必须配置新的前缀)')
+                }
+                if (config.srcFolderPath != null && config.srcFolderPath.trim().length() > 0) {
+                    sourceFolder = config.srcFolderPath
+                }
+                if (config.resFolderPath != null && config.resFolderPath.trim().length() > 0) {
+                    resFolder = config.resFolderPath
+                }
+                if (config.manifestFilePath != null && config.manifestFilePath.trim().length() > 0) {
+                    manifestFilePath = config.manifestFilePath
+                }
+
+                // === print all settings
+
+                println(">>>>>> old_prefix: ${config.old_prefix}")
+                println(">>>>>> new_prefix: ${config.new_prefix}")
+                println(">>>>>> srcFolder : ${sourceFolder}")
+                println(">>>>>> resFolder : ${resFolder}")
+                println(">>>>>> AndroidManifest.xml file path : ${manifestFilePath}")
+
+                // === do work
+                println "++++++++++++++++++++++ Start replace Android resources..."
+
+                ResToolsConfiguration workConfig = new ResToolsConfiguration(
+                        config.new_prefix,
+                        config.old_prefix,
+                        sourceFolder,
+                        resFolder,
+                        manifestFilePath
+                )
+                doWork(workConfig)
+
+                println("++++++++++++++++++++++ Finish replace resouces name, Total time: ${(System.currentTimeMillis() - startTime) / 1000} ")
+
             }
-
-            if (project.resConfig == null) {       // check config
-                throw new IllegalArgumentException(
-                        'ResTools gradle plugin "resConfig DSL" config can not be null.')
-            }
-
-            // === System default
-            String sourceFolder = project.android.sourceSets.main.java.srcDirs[0].getAbsolutePath()
-            String resFolder =    project.android.sourceSets.main.res.srcDirs[0].getAbsolutePath()
-            String manifestFilePath = project.android.sourceSets.main.manifest.srcFile.getAbsolutePath()
-
-            long startTime = System.currentTimeMillis()     // startTime
-
-            // === User settings
-            def config = project.resConfig
-            if (config.new_prefix == null || config.new_prefix.trim().length() == 0) {
-                throw new IllegalArgumentException(
-                        'the [new_prefix] can not be null (必须配置新的前缀)')
-            }
-            if (config.srcFolderPath != null && config.srcFolderPath.trim().length() > 0) {
-                sourceFolder = config.srcFolderPath
-            }
-            if (config.resFolderPath != null && config.resFolderPath.trim().length() > 0) {
-                resFolder = config.resFolderPath
-            }
-            if (config.manifestFilePath != null && config.manifestFilePath.trim().length() > 0) {
-                manifestFilePath = config.manifestFilePath
-            }
-
-            // === print all settings
-
-            println(">>>>>> old_prefix: ${config.old_prefix}")
-            println(">>>>>> new_prefix: ${config.new_prefix}")
-            println(">>>>>> srcFolder : ${sourceFolder}")
-            println(">>>>>> resFolder : ${resFolder}")
-            println(">>>>>> AndroidManifest.xml file path : ${manifestFilePath}")
-
-            // === do work
-            println "++++++++++++++++++++++ Start replace Android resources..."
-
-            ResToolsConfiguration workConfig = new ResToolsConfiguration(
-                    config.new_prefix,
-                    config.old_prefix,
-                    sourceFolder,
-                    resFolder,
-                    manifestFilePath
-            )
-            doWork(workConfig)
-
-            println("++++++++++++++++++++++ Finish replace resouces name, Total time: ${(System.currentTimeMillis() - startTime) / 1000} ")
         }
     }
 
